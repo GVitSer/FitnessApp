954f37c83df4e555e4444c3551ed029818a65878
GVitSer <gvs.bzn@mail.ru>
2020-10-04 20:09:24 +0300

    Add app FitnessApp
    *Create class library.
    *Create cmd file (main).
    *Creating a Link between the class library and the cmd file.
    *Create folder model.
    *Create class User.
    *Create class Gender.

---
+++ FItnessApp.BL/Model/Gender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace FItnessApp.BL.Model
+{
+    public class Gender
+    {
+        public string Name { get; }
+        public Gender (String name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("")
+            }
+        }
+    }
+}
